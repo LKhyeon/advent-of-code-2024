8b2a0bbb37e3dd0bfbbeb28664e310b03cd15a89
Donghyeon (Luke) Kim <43277040+LKhyeon@users.noreply.github.com>
2024-12-05 02:49:00 -0500

    Using linear alg

--- day_4/ceres_search_x_mas.py
+++ day_4/ceres_search_x_mas.py
@@ -18,15 +18,41 @@ SEARCH_MATRIX_2 = [['S', None ,'S'], [None, 'A', None], ['M', None, 'M']]
 SEARCH_MATRIX_3 = [['S', None ,'M'], [None, 'A', None], ['S', None, 'M']]
 SEARCH_MATRIX_4 = [['M', None ,'M'], [None, 'A', None], ['S', None, 'S']]
 
+# http://motion.pratt.duke.edu/RoboticSystems/CoordinateTransformations.html
+# https://math.stackexchange.com/questions/1676441/how-to-rotate-the-positions-of-a-matrix-by-90-degrees
+def rotate_matrix_cw(matrix):
+    # Reverse
+    flipped = []
+    for x in range(len(matrix)):
+        row = []
+        for y in reversed(range(len(matrix[0]))):
+            row.append(matrix[x][y])
+        flipped.append(row)
+
+    # Transpose
+    transposed = []
+    for y in range(len(flipped[0])):
+        row = []
+        for x in range(len(flipped)):
+            row.append(flipped[x][y])
+        transposed.append(row)
+    
+    return transposed
+
 def match(x, y):
     matched_one = False
-    for target in [SEARCH_MATRIX_1, SEARCH_MATRIX_2, SEARCH_MATRIX_3, SEARCH_MATRIX_4]:
-        center = MATRIX[x][y] == target[1][1]
-        north_east = MATRIX[x+1][y-1] == target[2][0]
-        south_east = MATRIX[x+1][y+1] == target[2][2]
-        south_west = MATRIX[x-1][y+1] == target[0][2]
-        north_west = MATRIX[x-1][y-1] == target[0][0]
+
+    rotated = SEARCH_MATRIX_1
+    for i in range(4):
+        center = MATRIX[y][x] == rotated[1][1]
+        north_east = MATRIX[y-1][x+1] == rotated[2][0]
+        south_east = MATRIX[y+1][x+1] == rotated[2][2]
+        south_west = MATRIX[y+1][x-1] == rotated[0][2]
+        north_west = MATRIX[y-1][x-1] == rotated[0][0]
         matched_one = matched_one or (center and north_east and south_east and south_west and north_west)
+
+        rotated = rotate_matrix_cw(rotated)
+        
     return int(matched_one)
 
 def search_submatrix():
